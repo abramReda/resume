1ae6c350d3fe6badfc25e194b8a79ca8ed1998a9
Ibram Reda <abramreda815@gmail.com>
2021-10-30 11:43:53 +0200

    my resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { WorkComponent } from './componets/work/work.component';
+import { AboutComponent } from './componets/about/about.component';
+import { HomeComponent } from './componets/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'work',
+    component:WorkComponent
+  },
+  {
+    path:'**',
+    redirectTo:'home'
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componets/home/home.component';
+import { AboutComponent } from './componets/about/about.component';
+import { WorkComponent } from './componets/work/work.component';
+import { SideBarComponent } from './componets/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
